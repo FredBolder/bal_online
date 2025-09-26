0ca992fcbb76c973dab0d917377719bd041148e0
Fred Bolder <bolder@muenzgalerie.de>
2025-09-26 13:17:06 +0200

    tests added/improved

---
+++ src/balUtils.dropObject.test.jsx
@@ -0,0 +1,178 @@
+import { describe, expect, test } from "vitest";
+import { dropObject } from "./balUtils.js";
+import { initGameInfo } from "./gameInfo.js";
+import { copy2dArray } from "./utils.js";
+
+describe("balUtils dropObject", () => {
+    const defaultGameInfo = {};
+    initGameInfo(defaultGameInfo);
+
+    test("dropObject A", () => {
+        const gameInfo = { ...defaultGameInfo, hasWhiteBall: true, blueBall: { x: 2, y: 3 } };
+        const input = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 2, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const expectedOutput = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 2, 4, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const info = dropObject(input, gameInfo, "whiteBall")
+        expect(input).toEqual(expectedOutput);
+        expect(info.update).toBe(true);
+        expect(gameInfo.hasWhiteBall).toBe(false);
+    });
+
+    test("dropObject B", () => {
+        const gameInfo = { ...defaultGameInfo, hasWhiteBall: true, blueBall: { x: 5, y: 3 } };
+        const input = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 2, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const expectedOutput = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 4, 2, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const info = dropObject(input, gameInfo, "whiteBall")
+        expect(input).toEqual(expectedOutput);
+        expect(info.update).toBe(true);
+        expect(gameInfo.hasWhiteBall).toBe(false);
+    });
+
+    test("dropObject C", () => {
+        const gameInfo = { ...defaultGameInfo, hasWhiteBall: false, blueBall: { x: 5, y: 3 } };
+        const input = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 2, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const expectedOutput = copy2dArray(input);
+        const info = dropObject(input, gameInfo, "whiteBall")
+        expect(input).toEqual(expectedOutput);
+        expect(info.update).toBe(false);
+        expect(gameInfo.hasWhiteBall).toBe(false);
+    });
+
+    test("dropObject D", () => {
+        const gameInfo = { ...defaultGameInfo, hasYellowBall: true, blueBall: { x: 4, y: 3 } };
+        const input = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 2, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const expectedOutput = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 2, 9, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const info = dropObject(input, gameInfo, "yellowBall")
+        expect(input).toEqual(expectedOutput);
+        expect(info.update).toBe(true);
+        expect(gameInfo.yellowBalls).toEqual([{ x: 5, y: 3, direction: "none" }]);
+        expect(gameInfo.hasYellowBall).toBe(false);
+    });
+
+    test("dropObject E", () => {
+        const gameInfo = { ...defaultGameInfo, yellowBalls: [{ x: 4, y: 2, direction: "none" }], hasYellowBall: true, blueBall: { x: 4, y: 3 } };
+        const input = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 9, 0, 1],
+            [1, 0, 0, 0, 2, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const expectedOutput = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 9, 0, 1],
+            [1, 0, 0, 0, 2, 9, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const info = dropObject(input, gameInfo, "yellowBall")
+        expect(input).toEqual(expectedOutput);
+        expect(info.update).toBe(true);
+        expect(gameInfo.yellowBalls).toEqual([{ x: 4, y: 2, direction: "none" }, { x: 5, y: 3, direction: "none" }]);
+        expect(gameInfo.hasYellowBall).toBe(false);
+    });
+
+    test("dropObject F", () => {
+        const gameInfo = { ...defaultGameInfo, hasLightBlueBall: true, blueBall: { x: 2, y: 3 } };
+        const input = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 2, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const expectedOutput = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 2, 5, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const info = dropObject(input, gameInfo, "lightBlueBall")
+        expect(input).toEqual(expectedOutput);
+        expect(info.update).toBe(true);
+        expect(gameInfo.hasLightBlueBall).toBe(false);
+    });
+
+    test("dropObject G", () => {
+        const gameInfo = { ...defaultGameInfo, hasLightBlueBall: true, blueBall: { x: 2, y: 3 } };
+        const input = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 1, 2, 1, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const expectedOutput = copy2dArray(input);
+        const info = dropObject(input, gameInfo, "lightBlueBall")
+        expect(input).toEqual(expectedOutput);
+        expect(info.update).toBe(false);
+        expect(gameInfo.hasLightBlueBall).toBe(true);
+    });
+
+    test("dropObject H", () => {
+        const gameInfo = { ...defaultGameInfo, hasWhiteBall: true, hasPurpleBall: true, blueBall: { x: 5, y: 3 } };
+        const input = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 2, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const expectedOutput = [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 28, 2, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ];
+        const info = dropObject(input, gameInfo, "purpleBall")
+        expect(input).toEqual(expectedOutput);
+        expect(info.update).toBe(true);
+        expect(gameInfo.hasPurpleBall).toBe(false);
+        expect(gameInfo.hasWhiteBall).toBe(true);
+    });
+
+    // Insert new tests here
+});
--- src/balUtils.moveRight.test.jsx
+++ src/balUtils.moveRight.test.jsx
@@ -12,347 +12,286 @@ describe("balUtils moveRight", () => {
   const defaultGameVars = {};
   initGameVars(defaultGameVars);
 
-  let gameInfo01a = { ...defaultGameInfo, blueBall: { x: 4, y: 1 } };
-  let inputBack01_3_6 = zeroArray(3, 6);
-  let input01a = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 4, 0, 2, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01a = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 4, 0, 2, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let info01a = moveRight(inputBack01_3_6, input01a, gameInfo01a, { ...defaultGameVars });
+  const inputBack01_3_6 = zeroArray(3, 6);
+  const inputBack01_3_7 = zeroArray(3, 7);
+  const inputBack01_8_6 = zeroArray(8, 6);
+
   it("moveRight A", () => {
-    expect(JSON.stringify(input01a)).toBe(JSON.stringify(expectedOutput01a));
-  });
-  it("moveRight A eating", () => {
-    expect(info01a.eating).toBe(false);
-  });
-  it("moveRight A player", () => {
-    expect(info01a.player).toBe(false);
-  });
-  it("moveRight A blueBall", () => {
-    expect(JSON.stringify(gameInfo01a.blueBall)).toBe(JSON.stringify({ x: 4, y: 1 }));
+    const gameInfo = { ...defaultGameInfo, blueBall: { x: 4, y: 1 } };
+    const input = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 4, 0, 2, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 4, 0, 2, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const info = moveRight(inputBack01_3_6, input, gameInfo, { ...defaultGameVars });
+    expect(input).toEqual(expectedOutput);
+    expect(info.eating).toBe(false);
+    expect(info.player).toBe(false);
+    expect(gameInfo.blueBall).toEqual({ x: 4, y: 1 });
   });
 
-  let gameInfo01b = { ...defaultGameInfo, blueBall: { x: 3, y: 1 } };
-  let input01b = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 4, 2, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01b = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 4, 0, 2, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let info01b = moveRight(inputBack01_3_6, input01b, gameInfo01b, { ...defaultGameVars });
   it("moveRight B", () => {
-    expect(JSON.stringify(input01b)).toBe(JSON.stringify(expectedOutput01b));
-  });
-  it("moveRight B info", () => {
-    expect(JSON.stringify(info01b)).toBe(
-      JSON.stringify({
+    const gameInfo = { ...defaultGameInfo, blueBall: { x: 3, y: 1 } };
+    const input = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 4, 2, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 4, 0, 2, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const info = moveRight(inputBack01_3_6, input, gameInfo, { ...defaultGameVars });
+    expect(input).toEqual(expectedOutput);
+    expect(info).toEqual(
+      {
         action: "",
         eating: false,
         freezeTime: -1,
         player: true,
         sound: "",
-      })
+      }
     );
-  });
-  it("moveRight B blueBall", () => {
-    expect(JSON.stringify(gameInfo01b.blueBall)).toBe(JSON.stringify({ x: 4, y: 1 }));
+    expect(gameInfo.blueBall).toEqual({ x: 4, y: 1 });
   });
 
-  let input01c = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 4, 2, 3, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01c = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 4, 0, 2, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let info01c = moveRight(inputBack01_3_6, input01c, { ...defaultGameInfo, blueBall: { x: 3, y: 1 } }, { ...defaultGameVars });
   it("moveRight C", () => {
-    expect(JSON.stringify(input01c)).toBe(JSON.stringify(expectedOutput01c));
-  });
-  it("moveRight C eating", () => {
-    expect(info01c.eating).toBe(true);
-  });
-  it("moveRight C player", () => {
-    expect(info01c.player).toBe(true);
+    const input = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 4, 2, 3, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 4, 0, 2, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const info = moveRight(inputBack01_3_6, input, { ...defaultGameInfo, blueBall: { x: 3, y: 1 } }, { ...defaultGameVars });
+    expect(input).toEqual(expectedOutput);
+    expect(info.eating).toBe(true);
+    expect(info.player).toBe(true);
   });
 
-  let input01d = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 2, 4, 4, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01d = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 2, 4, 4, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let info01d = moveRight(inputBack01_3_6, input01d, { ...defaultGameInfo, blueBall: { x: 1, y: 1 } }, { ...defaultGameVars });
   it("moveRight D", () => {
-    expect(JSON.stringify(input01d)).toBe(JSON.stringify(expectedOutput01d));
-  });
-  it("moveRight D eating", () => {
-    expect(info01d.eating).toBe(false);
-  });
-  it("moveRight D player", () => {
-    expect(info01d.player).toBe(true);
+    const input = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 2, 4, 4, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 2, 4, 4, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const info = moveRight(inputBack01_3_6, input, { ...defaultGameInfo, blueBall: { x: 1, y: 1 } }, { ...defaultGameVars });
+    expect(input).toEqual(expectedOutput);
+    expect(info.eating).toBe(false);
+    expect(info.player).toBe(true);
   });
 
-  let input01e = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 2, 4, 4, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01e = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 2, 4, 4, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let info01e = moveRight(inputBack01_3_6, input01e, { ...defaultGameInfo, blueBall: { x: 2, y: 1 } }, { ...defaultGameVars });
   it("moveRight E", () => {
-    expect(JSON.stringify(input01e)).toBe(JSON.stringify(expectedOutput01e));
-  });
-  it("moveRight E eating", () => {
-    expect(info01e.eating).toBe(false);
-  });
-  it("moveRight E player", () => {
-    expect(info01e.player).toBe(false);
+    const input = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 2, 4, 4, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 2, 4, 4, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const info = moveRight(inputBack01_3_6, input, { ...defaultGameInfo, blueBall: { x: 2, y: 1 } }, { ...defaultGameVars });
+    expect(input).toEqual(expectedOutput);
+    expect(info.eating).toBe(false);
+    expect(info.player).toBe(false);
   });
 
-  let inputBack01_3_7 = zeroArray(3, 7);
-  let input01f = [
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 4, 4, 4, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01f = [
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 4, 4, 4, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1],
-  ];
-  let info01f = moveRight(inputBack01_3_7, input01f, { ...defaultGameInfo, blueBall: { x: 1, y: 1 } }, { ...defaultGameVars });
   it("moveRight F", () => {
-    expect(JSON.stringify(input01f)).toBe(JSON.stringify(expectedOutput01f));
-  });
-  it("moveRight F eating", () => {
-    expect(info01f.eating).toBe(false);
-  });
-  it("moveRight F player", () => {
-    expect(info01f.player).toBe(false);
+    const input = [
+      [1, 1, 1, 1, 1, 1, 1],
+      [1, 2, 4, 4, 4, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput = [
+      [1, 1, 1, 1, 1, 1, 1],
+      [1, 2, 4, 4, 4, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1],
+    ];
+    const info = moveRight(inputBack01_3_7, input, { ...defaultGameInfo, blueBall: { x: 1, y: 1 } }, { ...defaultGameVars });
+    expect(input).toEqual(expectedOutput);
+    expect(info.eating).toBe(false);
+    expect(info.player).toBe(false);
   });
 
-  let input01g = [
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 4, 5, 4, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01g = [
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 4, 5, 4, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1],
-  ];
-  let info01g = moveRight(inputBack01_3_7, input01g, { ...defaultGameInfo, blueBall: { x: 1, y: 1 } }, { ...defaultGameVars });
   it("moveRight G", () => {
-    expect(JSON.stringify(input01g)).toBe(JSON.stringify(expectedOutput01g));
-  });
-  it("moveRight G eating", () => {
-    expect(info01g.eating).toBe(false);
-  });
-  it("moveRight G player", () => {
-    expect(info01g.player).toBe(false);
+    const input = [
+      [1, 1, 1, 1, 1, 1, 1],
+      [1, 2, 4, 5, 4, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput = [
+      [1, 1, 1, 1, 1, 1, 1],
+      [1, 2, 4, 5, 4, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1],
+    ];
+    const info = moveRight(inputBack01_3_7, input, { ...defaultGameInfo, blueBall: { x: 1, y: 1 } }, { ...defaultGameVars });
+    expect(input).toEqual(expectedOutput);
+    expect(info.eating).toBe(false);
+    expect(info.player).toBe(false);
   });
 
-  let input01h = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 2, 5, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01h = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 2, 5, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let info01h = moveRight(inputBack01_3_6, input01h, { ...defaultGameInfo, blueBall: { x: 1, y: 1 } }, { ...defaultGameVars });
   it("moveRight H", () => {
-    expect(JSON.stringify(input01h)).toBe(JSON.stringify(expectedOutput01h));
-  });
-  it("moveRight H eating", () => {
-    expect(info01h.eating).toBe(false);
-  });
-  it("moveRight H player", () => {
-    expect(info01h.player).toBe(true);
+    const input = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 2, 5, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 2, 5, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const info = moveRight(inputBack01_3_6, input, { ...defaultGameInfo, blueBall: { x: 1, y: 1 } }, { ...defaultGameVars });
+    expect(input).toEqual(expectedOutput);
+    expect(info.eating).toBe(false);
+    expect(info.player).toBe(true);
   });
 
-  let input01i = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 2, 28, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01i = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 2, 28, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let info01i = moveRight(inputBack01_3_6, input01i, { ...defaultGameInfo, blueBall: { x: 1, y: 1 } }, { ...defaultGameVars });
   it("moveRight I", () => {
-    expect(JSON.stringify(input01i)).toBe(JSON.stringify(expectedOutput01i));
-  });
-  it("moveRight I eating", () => {
-    expect(info01i.eating).toBe(false);
-  });
-  it("moveRight I player", () => {
-    expect(info01i.player).toBe(true);
+    const input = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 2, 28, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 2, 28, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const info = moveRight(inputBack01_3_6, input, { ...defaultGameInfo, blueBall: { x: 1, y: 1 } }, { ...defaultGameVars });
+    expect(input).toEqual(expectedOutput);
+    expect(info.eating).toBe(false);
+    expect(info.player).toBe(true);
   });
 
-  let input01j = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 2, 9, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01j = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 2, 9, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let yellow01j = [{ x: 2, y: 1, direction: "none" }];
-  let info01j = moveRight(inputBack01_3_6, input01j, { ...defaultGameInfo, blueBall: { x: 1, y: 1 }, yellowBalls: yellow01j }, { ...defaultGameVars });
   it("moveRight J", () => {
-    expect(JSON.stringify(input01j)).toBe(JSON.stringify(expectedOutput01j));
-  });
-  it("moveRight J eating", () => {
+    const input01j = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 2, 9, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput01j = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 2, 9, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const yellow01j = [{ x: 2, y: 1, direction: "none" }];
+    const info01j = moveRight(inputBack01_3_6, input01j, { ...defaultGameInfo, blueBall: { x: 1, y: 1 }, yellowBalls: yellow01j }, { ...defaultGameVars });
+    expect(input01j).toEqual(expectedOutput01j);
     expect(info01j.eating).toBe(false);
-  });
-  it("moveRight J player", () => {
     expect(info01j.player).toBe(true);
-  });
-  it("moveRight J yellow", () => {
-    expect(JSON.stringify(yellow01j)).toBe(
-      JSON.stringify([{ x: 3, y: 1, direction: "right" }])
-    );
+    expect(yellow01j).toEqual([{ x: 3, y: 1, direction: "right" }]);
   });
 
-  let inputBack01_8_6 = zeroArray(8, 6);
-
-  let input01k = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 3, 0, 2, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 109, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01k = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 3, 0, 0, 2, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 109, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let info01k = moveRight(inputBack01_8_6, input01k, { ...defaultGameInfo, blueBall: { x: 3, y: 1 }, forces: [{ x: 3, y: 6, direction: "up" }] }, { ...defaultGameVars });
   it("moveRight K", () => {
-    expect(JSON.stringify(input01k)).toBe(JSON.stringify(expectedOutput01k));
-  });
-  it("moveRight K player", () => {
-    expect(info01k.player).toBe(true);
+    const input = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 3, 0, 2, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 109, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 3, 0, 0, 2, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 109, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const info = moveRight(inputBack01_8_6, input, { ...defaultGameInfo, blueBall: { x: 3, y: 1 }, forces: [{ x: 3, y: 6, direction: "up" }] }, { ...defaultGameVars });
+    expect(input).toEqual(expectedOutput);
+    expect(info.player).toBe(true);
   });
 
-  let input01l = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 3, 0, 0, 0, 1],
-    [1, 0, 0, 2, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 109, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01l = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 3, 0, 0, 0, 1],
-    [1, 0, 0, 2, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 109, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let info01l = moveRight(inputBack01_8_6, input01l, { ...defaultGameInfo, blueBall: { x: 3, y: 2 }, forces: [{ x: 3, y: 6, direction: "up" }] }, { ...defaultGameVars });
   it("moveRight L", () => {
-    expect(JSON.stringify(input01l)).toBe(JSON.stringify(expectedOutput01l));
-  });
-  it("moveRight L player", () => {
-    expect(info01l.player).toBe(false);
+    const input = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 3, 0, 0, 0, 1],
+      [1, 0, 0, 2, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 109, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 3, 0, 0, 0, 1],
+      [1, 0, 0, 2, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 0, 109, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const info = moveRight(inputBack01_8_6, input, { ...defaultGameInfo, blueBall: { x: 3, y: 2 }, forces: [{ x: 3, y: 6, direction: "up" }] }, { ...defaultGameVars });
+    expect(input).toEqual(expectedOutput);
+    expect(info.player).toBe(false);
   });
 
-  let input01m = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 2, 5, 0, 112, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01m = copy2dArray(input01m);
-  let info01m = moveRight(inputBack01_3_6, input01m, { ...defaultGameInfo, blueBall: { x: 1, y: 1 }, forces: [{ x: 4, y: 1, direction: "left" }] }, { ...defaultGameVars });
   it("moveRight M", () => {
-    expect(JSON.stringify(input01m)).toBe(JSON.stringify(expectedOutput01m));
-  });
-  it("moveRight M player", () => {
-    expect(info01m.player).toBe(false);
+    const input = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 2, 5, 0, 112, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput = copy2dArray(input);
+    const info = moveRight(inputBack01_3_6, input, { ...defaultGameInfo, blueBall: { x: 1, y: 1 }, forces: [{ x: 4, y: 1, direction: "left" }] }, { ...defaultGameVars });
+    expect(input).toEqual(expectedOutput);
+    expect(info.player).toBe(false);
   });
 
-  let gameInfo01n = { ...defaultGameInfo, blueBall: { x: 2, y: 2 }, yellowBars: [{ x: 3, y: 2, direction: "none" }] };
-  let input01n = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 3, 0, 0, 0, 1],
-    [1, 0, 2, 124, 0, 1],
-    [1, 0, 5, 123, 0, 1],
-    [1, 0, 0, 123, 0, 1],
-    [1, 0, 0, 125, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let expectedOutput01n = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 3, 0, 0, 0, 1],
-    [1, 0, 0, 2, 124, 1],
-    [1, 0, 5, 0, 123, 1],
-    [1, 0, 0, 0, 123, 1],
-    [1, 0, 0, 0, 125, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-  ];
-  let info01n = moveRight(inputBack01_8_6, input01n, gameInfo01n, { ...defaultGameVars });
   it("moveRight N", () => {
-    expect(JSON.stringify(input01n)).toBe(JSON.stringify(expectedOutput01n));
+    const gameInfo = { ...defaultGameInfo, blueBall: { x: 2, y: 2 }, yellowBars: [{ x: 3, y: 2, direction: "none" }] };
+    const input = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 3, 0, 0, 0, 1],
+      [1, 0, 2, 124, 0, 1],
+      [1, 0, 5, 123, 0, 1],
+      [1, 0, 0, 123, 0, 1],
+      [1, 0, 0, 125, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const expectedOutput = [
+      [1, 1, 1, 1, 1, 1],
+      [1, 3, 0, 0, 0, 1],
+      [1, 0, 0, 2, 124, 1],
+      [1, 0, 5, 0, 123, 1],
+      [1, 0, 0, 0, 123, 1],
+      [1, 0, 0, 0, 125, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1],
+    ];
+    const info = moveRight(inputBack01_8_6, input, gameInfo, { ...defaultGameVars });
+    expect(input).toEqual(expectedOutput);
+    expect(info.eating).toBe(false);
+    expect(info.player).toBe(true);
+    expect(gameInfo.blueBall).toEqual({ x: 3, y: 2 });
+    expect(gameInfo.yellowBars).toEqual([{ x: 4, y: 2, direction: "right" }]);
   });
-  it("moveRight N eating", () => {
-    expect(info01n.eating).toBe(false);
-  });
-  it("moveRight N player", () => {
-    expect(info01n.player).toBe(true);
-  });
-  it("moveRight N blueBall", () => {
-    expect(JSON.stringify(gameInfo01n.blueBall)).toBe(JSON.stringify({ x: 3, y: 2 }));
-  });
-  it("moveRight N yellowBars", () => {
-    expect(JSON.stringify(gameInfo01n.yellowBars)).toBe(JSON.stringify([{ x: 4, y: 2, direction: "right" }]));
-  });
-
 
   // Insert new tests here
 });
