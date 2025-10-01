98e79c0c9f74b4a16d7dab0b75fb85024df7b663
Fred Bolder <bolder@muenzgalerie.de>
2025-10-01 21:45:45 +0200

    level added

--- src/Components/BalPage.jsx
+++ src/Components/BalPage.jsx
@@ -1763,7 +1763,7 @@ function BalPage() {
       gameVars.currentLevel = 200;
       loadProgress();
       if (fred) {
-        gameVars.currentLevel = 3308;
+        gameVars.currentLevel = 3309;
       }
       initLevel(gameVars.currentLevel);
     }
