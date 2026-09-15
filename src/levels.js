398f5d803fe0158ae27c92bb4961ee0d9d25d032
Fred Bolder <bolder@muenzgalerie.de>
2026-09-15 21:18:28 +0200

    level added

--- src/levels.js
+++ src/levels.js
@@ -59,7 +59,7 @@ export const seriesFishEnd = 6365;
 export const seriesProgrammingStart = 6400;
 export const seriesProgrammingEnd = 6402;
 export const seriesAnnoyingStart = 6450;
-export const seriesAnnoyingEnd = 6457;
+export const seriesAnnoyingEnd = 6458;
 
 export function addSolvedLevels(levelStr) {
   let level = -1;
